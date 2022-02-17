80692e9c3958d646e2e8c40a2e082cc4ae0e5328
Sorin <work@csorin.com>
2022-02-17 15:07:37 +0100

    feat: change theme

---
+++ components/AppFooter.vue
@@ -0,0 +1,35 @@
+<template>
+  <footer
+    class="h-16 relative w-full border-t dark:border-gray-800 bg-white dark:bg-gray-900 z-20"
+  >
+    <div
+      class="h-full w-full container mx-auto px-4 lg:px-8 flex items-center justify-between"
+    >
+      <div class="flex items-start flex-col">
+        <div class="text-lg">Blog</div>
+        <a
+          href="https://nuxtjs.org"
+          target="_blank"
+          rel="noopener"
+          class="text-sm text-gray-500"
+        >
+          Built with Nuxt
+        </a>
+      </div>
+      <div class="flex items-center space-x-4">
+        <AppLangSwitcher />
+        <AppColorSwitcher />
+      </div>
+    </div>
+  </footer>
+</template>
+
+<script>
+export default {
+  computed: {
+    availableLocales() {
+      return this.$i18n.locales.filter((i) => i.code !== this.$i18n.locale);
+    },
+  },
+};
+</script>
---
+++ nuxt.config.js
@@ -0,0 +1,10 @@
+import theme from "@nuxt/content-theme-docs";
+
+export default theme({
+  docs: {
+    primaryColor: "#0073e6",
+  },
+  head: {
+    link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }],
+  },
+});
